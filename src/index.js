b49c6b2b46ad332bcb634a80963aea39226f7725
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-27 17:39:41 +0500

    Bundle .js file

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { helloWebpack } from "./js/hello-webpack.js";
+
+console.log(helloWebpack);
---
+++ src/js/hello-webpack.js
@@ -0,0 +1,4 @@
+import { stringHello } from "./string-hello.js";
+import { stringWebpack } from "./string-webpack.js";
+
+export const helloWebpack = `${stringHello} ${stringWebpack}!`;
