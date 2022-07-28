b0bc2087cc0f943d8073fa35a34d3f97b5c1c496
Yury Olennikov <OlennikovYV@mail.ru>
2022-07-28 17:44:54 +0500

    The env variable has been added. Its value is used to select different CSS modules. Added plugins: mini-css-extract-plugin, clean-webpack-plugin and copy-webpack-plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
 const path = require("path");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const env = process.env.NODE_ENV;
+const cssLoaders =
+  env === "prod"
+    ? [MiniCSSExtractPlugin.loader, "css-loader"]
+    : ["style-loader", "css-loader"];
 
 const config = {
   mode: "development",
@@ -9,10 +18,24 @@ const config = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: cssLoaders,
       },
     ],
   },
+  plugins: [
+    new MiniCSSExtractPlugin({
+      filename: "style.css",
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: "",
+        },
+      ],
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, "build/"),
     filename: "bundle.js",
